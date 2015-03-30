94cd253c81cd097426637a7fd75b5f15687d821f
Orlin M Bozhinov <om@soundsapiens.com>
2015-03-30 18:13:19 +0300

    beverage gulp, build with coffee-inline-map

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,12 @@
-var gulp = require('gulp-npm-run')(require('gulp'), {
-  exclude: ['test'],
-  include: {'build': 'Compile coffee with inline source-map.'},
-  require: ['build'],
-  requireStrict: true
+var gulp = require('beverage')(require('gulp'), {
+  scripts: {
+    include: {'build': 'Compile coffee with inline source-map.'}
+  },
+  buildWatch: ['*.coffee'],
+  test: {
+    testsRe: /\.spec\.coffee$/,
+  },
+  testWatch: ['index.js', 'test/*.spec.coffee']
 })
 
-gulp.task('build:watch', function(){
-  gulp.watch('*.coffee', ['build'])
-})
-
-// modify 'test'; reuse test fn for gulp test:watch
-var test = require('gulp-npm-test')(gulp, {
-  testsRe: /\.spec\.coffee$/,
-  testCmd: 'node_modules/.bin/mocha'
-})
-
-gulp.task('test:watch', function() {
-  require('gulp-watch')(['index.js', 'test/*.spec.coffee'], test)
-})
-
-gulp.task('dev', 'Develop ...', ['build', 'build:watch', 'test:watch'])
+gulp.task('dev', 'DEVELOP', ['build', 'build:watch', 'test:watch'])
