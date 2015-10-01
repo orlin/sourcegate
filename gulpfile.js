6fdc3153e84fa014676dfa3c177ce3a1df3d9b48
Orlin M Bozhinov <om@soundsapiens.com>
2015-10-01 21:51:20 +0300

    upgrade beverage corage config, the gulp-npm-test warning left on purpose to demonstrate an issue with beverage disrespecting the validity of gulp-npm-run taking care of tests, gulp-npm-test use shouldn't be forced

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,7 @@
-var gulp = require('beverage')(require('gulp'), {
-  scripts: {
-    include: {'build': 'Compile coffee with inline source-map.'}
-  },
-  buildWatch: ['index.coffee'],
-  test: {},
-  testWatch: ['index.js', 'test/*.spec.coffee']
-})
+var gulp = require('beverage')(require('gulp'))
 
 gulp.task('dev', 'DEVELOP', [
   'build',
-  'test',
-  'sourcegate:watch',
   'build:watch',
   'test:watch'
 ])
